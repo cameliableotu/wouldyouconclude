98cba4e1b3cf74af7f10bbe5bb015ac64ff2c021
Adina Camelia Bleotu <53470693+cameliableotu@users.noreply.github.com>
2020-04-04 11:08:08 -0400

    Update ArchiteuthisDux_One.js

--- data_includes/ArchiteuthisDux_One.js
+++ data_includes/ArchiteuthisDux_One.js
@@ -69,7 +69,7 @@ function modifyRunningOrder(ro)
 
 // Items array.
 var items = [
-["timeoutSep", Separator, { transfer: 250, normalMessage: "", errorMessage: "Timed out. Vă rugăm să răspundeți mai rapid."}],
+["timeoutSep", Separator, { transfer: 2500, normalMessage: "", errorMessage: "Timed out. Vă rugăm să răspundeți mai rapid."}],
 
 ["consent", "Form", {consentRequired: true, html: {include: "consent.html"}}],
  ["setcounter", "__SetCounter__", { }],
